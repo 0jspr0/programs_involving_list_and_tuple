d8b12fef02bcaee958eb0aa45b06220e8efa5b71
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-03-15 23:40:37 +0800

    Added an if-else statement to identify if going into the unique list or the duplicate list

--- with_duplicate.py
+++ with_duplicate.py
@@ -5,3 +5,7 @@ duplicate_list = []
 
 for i in range(10):
     number = int(input(f"Input number {i + 1}: "))
+    if number not in unique_list:
+        unique_list.append(number)
+    else:
+        duplicate_list.append(number)
