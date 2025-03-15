7cd384b0f987cd59243de0908cabb54311acd162
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-03-15 23:47:35 +0800

    Initialize a with duplicate list

--- with_duplicate.py
+++ with_duplicate.py
@@ -3,6 +3,8 @@ print("Input 10 numbers to display the numbers with duplicates")
 unique_list = []
 duplicate_list = []
 
+with_duplicate_list = []
+
 for i in range(10):
     number = int(input(f"Input number {i + 1}: "))
     if number not in unique_list:
